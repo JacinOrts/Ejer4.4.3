a66141b224d1e014f8b4fd187cd5dba52d052936
Jacinto Orts <jacinto.orts@gmail.com>
2023-02-03 20:09:53 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer4._4._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name ="Quentin";
+            int x = 3;
+            x = x * 17;
+            double d = Math.PI / 2;
+            MessageBox.Show("name is " + name + "\nx is " + x +"\nd is " + d);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            while (cont < 10)
+            {
+                cont = cont + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                cont = cont - 1;
+            }
+
+            MessageBox.Show("La respuesta es: " + cont);
+
+        }
+    }
+}
